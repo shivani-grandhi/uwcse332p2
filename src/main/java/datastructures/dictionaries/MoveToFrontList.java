9749b577a67c0fb82e5abf327080974bc93ce52f
Shivani Grandhi <shivag2@uw.edu>
2023-01-26 20:48:27 -0800

    checkpoint1

--- src/main/java/datastructures/dictionaries/MoveToFrontList.java
+++ src/main/java/datastructures/dictionaries/MoveToFrontList.java
@@ -3,8 +3,10 @@ package datastructures.dictionaries;
 import cse332.datastructures.containers.Item;
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.misc.DeletelessDictionary;
+import cse332.interfaces.misc.SimpleIterator;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * 1. The list is typically not sorted.
@@ -20,18 +22,85 @@ import java.util.Iterator;
  * dictionary/list's iterator.
  */
 public class MoveToFrontList<K, V> extends DeletelessDictionary<K, V> {
+    private FrontList front;
+
+    private class FrontList {
+        private Item<K, V> data;
+        private FrontList next;
+        public FrontList(Item<K, V> item) {
+            this(item, null);
+        }
+
+        public FrontList(Item<K, V> item, FrontList next) {
+            this.data = item;
+            this.next = next;
+        }
+    }
     @Override
     public V insert(K key, V value) {
-        throw new NotYetImplementedException();
+        if (key == null || value == null) {
+            throw new IllegalArgumentException();
+        }
+        V prev = this.find(key);
+        if (prev != null) {
+            this.front.data.value = value;
+        } else {
+            this.front = new FrontList(new Item(key, value), this.front);
+            this.size++;
+        }
+        return prev;
     }
 
     @Override
     public V find(K key) {
-        throw new NotYetImplementedException();
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        FrontList current = this.front;
+        V returnValue = null;
+        if (current != null && current.data != null) {
+            if (current.data.key.equals(key)) {
+                return current.data.value;
+            }
+            while (current.next != null && current.next.data != null &&
+                    !current.next.data.key.equals(key)) {
+                current = current.next;
+            }
+            if (current.next != null && current.next.data != null) {
+                returnValue = current.next.data.value;
+                FrontList temp = current.next;
+                current.next = temp.next;
+                temp.next = this.front;
+                this.front = temp;
+            }
+        }
+        return returnValue;
     }
 
     @Override
     public Iterator<Item<K, V>> iterator() {
-        throw new NotYetImplementedException();
+        return new ListIterator();
+    }
+
+    private class ListIterator extends SimpleIterator<Item<K, V>> {
+        private FrontList current;
+
+        public ListIterator() {
+            this.current = MoveToFrontList.this.front;
+        }
+
+        public boolean hasNext() {
+            return current != null && current.next != null;
+        }
+
+        public Item<K, V> next() {
+            if(!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Item<K, V> returnItem = current.data;
+            this.current = current.next;
+            return returnItem;
+        }
+
     }
 }
--- src/main/java/datastructures/worklists/MinFourHeapComparable.java
+++ src/main/java/datastructures/worklists/MinFourHeapComparable.java
@@ -3,6 +3,9 @@ package datastructures.worklists;
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.worklists.PriorityWorkList;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 /**
  * See cse332/interfaces/worklists/PriorityWorkList.java
  * for method specifications.
@@ -10,38 +13,102 @@ import cse332.interfaces.worklists.PriorityWorkList;
 public class MinFourHeapComparable<E extends Comparable<E>> extends PriorityWorkList<E> {
     /* Do not change the name of this field; the tests rely on it to work correctly. */
     private E[] data;
+    private int capacity;
+    private int currentSize;
 
     public MinFourHeapComparable() {
-        throw new NotYetImplementedException();
+        currentSize = 0;
+        capacity = 100;
+        data = (E[]) new Comparable[capacity];
     }
 
     @Override
     public boolean hasWork() {
-        throw new NotYetImplementedException();
+        return super.hasWork();
     }
 
     @Override
     public void add(E work) {
-        throw new NotYetImplementedException();
+        if (currentSize == capacity) {
+            capacity = capacity * 2;
+            data = Arrays.copyOf(data, capacity);
+        }
+        data[currentSize] = work;
+        if (this.currentSize > 0) {
+            int parent = (this.currentSize - 1)/4;
+            percolateUp(parent, this.currentSize);
+        }
+        currentSize++;
+    }
+
+    private void percolateUp(int parent, int child) {
+        while (child != 0 && this.data[child].compareTo(this.data[parent]) < 0) {
+            E temp = this.data[child];
+            this.data[child] = this.data[parent];
+            this.data[parent] = temp;
+            child = parent;
+            parent = (child - 1) / 4;
+        }
     }
 
     @Override
     public E peek() {
-        throw new NotYetImplementedException();
+        if (!hasWork()) {
+            throw new NoSuchElementException();
+        }
+        return data[0];
     }
 
     @Override
     public E next() {
-        throw new NotYetImplementedException();
+        if (!this.hasWork()) {
+            throw new NoSuchElementException();
+        }
+        E value = this.data[0];
+        this.data[0] = this.data[this.currentSize - 1];
+        this.currentSize--;
+        if (this.currentSize > 0) {
+            int min = 1;
+            for (int i = min + 1; i < min + 4; i++) {
+                if (i < this.currentSize && this.data[i].compareTo(this.data[min]) < 0) {
+                    min = i;
+                }
+            }
+            percolateDown(0, min);
+        }
+        return value;
+    }
+
+    private void percolateDown(int parent, int child) {
+        while (this.data[parent].compareTo(this.data[child]) > 0) {
+
+            E temp = this.data[child];
+            this.data[child] = this.data[parent];
+            this.data[parent] = temp;
+            parent = child;
+            int first = 4 * parent + 1;
+            int min = first;
+            if (first > this.currentSize - 1)
+                break;
+            for (int i = first + 1; i < first + 4; i++) {
+                if (i < this.currentSize && this.data[i].compareTo(this.data[min]) < 0) {
+                    min = i;
+                }
+            }
+            child = min;
+        }
+
     }
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return this.currentSize;
     }
 
     @Override
     public void clear() {
-        throw new NotYetImplementedException();
+        capacity = 10;
+        data = (E[]) new Comparable[capacity];
+        currentSize = 0;
     }
 }
